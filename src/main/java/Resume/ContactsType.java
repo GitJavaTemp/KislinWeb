7b56c2299c23bc68b77543cb8792e43ba61a1fea
GitJavaTemp <benz-classik@yandex.ru>
2021-03-24 12:04:38 +0300

    lection 3.1

--- src/main/java/Resume/ContactsType.java
+++ src/main/java/Resume/ContactsType.java
@@ -5,11 +5,20 @@ package Resume;
  * 07.03.2021
  */
 public enum ContactsType {
-    EMAIL,
-    ADRESS,
-    MOBILENUMBER,
-    HOMENUMBER,
-    VIBER,
-    SKYPE
+    EMAIL("Электронная почта"),
+    ADRESS("Место жительства"),
+    MOBILENUMBER("Моильный тел."),
+    HOMENUMBER("Домашний тел."),
+    VIBER("Вайбер"),
+    SKYPE("Скайп");
 
+    private String title;
+
+    ContactsType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
